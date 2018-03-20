13414e318618e456fa0539f45fca25231ce1f423
sapofree <B.Ramos@ua.pt>
2018-03-20 17:56:52 +0000

    código SQL adicionado, copiado código simulador

---
+++ Clientes/Cliente01/src/simulador/prototypes.h
@@ -0,0 +1,14 @@
+/*File generated automatically in sapofree-Lenovo-ideapad-110-15IBR by sapofree on Qua Fev  7 13:42:00 WET 2018*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* simulador.c */
+int main (void);
+/* myf.c */
+void CREATE_DROP_TABLES (void);
+void InterceptCTRL_C (int a);
+void QUERY_INSERT_INTO_TABLE (void);
+void START_CYCLE (void);
+#ifdef __cplusplus
+}
+#endif
