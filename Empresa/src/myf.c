69ca4704e56e5be447778d99f9d3b3dc39d4100c
sapofree <B.Ramos@ua.pt>
2018-03-21 12:33:34 +0000

    Criado programa para transferência dos  dados

---
+++ Empresa/src/myf.c
@@ -0,0 +1,41 @@
+/**
+ *       @file  myf.c
+ *      @brief  Contém as funções para a execução do ciclo infinito
+ *
+ * 
+ *
+ *     @author  Bruno Ramos, B.Ramos@ua.pt
+ *
+ *   @internal
+ *     Created  20-Mar-2018
+ *     Company  University of Aveiro
+ *   Copyright  Copyright (c) 2018, Bruno Ramos
+ *
+ * =====================================================================================
+ */
+
+#include "myf.h"
+
+/**
+ * @brief Ciclo infinito a cada ciclo ele introduz um valor na base de dados e a cada cinco ciclos ele atualiza as tabelas
+ * @param void
+ * @return void
+ */
+void START_CYCLE(void)
+{
+	do
+	{
+		//QUERY_INSERT_INTO_TABLE();
+		//intervalo entre valores obtidos, actualmente 1s
+		//usleep(200000);
+		//Variável alterada na callback ctrl+c
+	} while (keep_goingG);
+}
+
+/**
+ * @brief  Callback para ctrl+c, desliga ciclo infinito
+ */
+void InterceptCTRL_C(int a)
+{
+	keep_goingG = 0;
+}
---
+++ Empresa/src/transferencia.c
@@ -0,0 +1,50 @@
+/**
+ *       @file  simulador.c
+ *      @brief  Simula as variáveis em tempo real e regista o seu valor na base de dados
+ *
+ * Simples conecção ao servidor MySQL baseado em: https://www.cyberciti.biz/tips/linux-unix-connect-mysql-c-api-program.html\n
+O programa só termina pressionando ctrl+c.
+ *
+ *     @author  Bruno Ramos, B.Ramos@ua.pt
+ *
+ *   @internal
+ *     Created  20-Mar-2018
+ *     Company  University of Aveiro
+ *   Copyright  Copyright (c) 2018, Bruno Ramos
+ *
+ * =====================================================================================
+ */
+#define _MAIN_C_
+
+#include <signal.h>
+#include "myf.h"
+
+static char *host = "127.0.0.1"; //ip da base de dados
+static char *user = "sapofree";
+static char *pass = "naopossodizer";
+static char *dbname = "central";
+
+unsigned int port = 3306;
+static char *unix_socket = NULL;
+unsigned int flag = 0;
+
+int main(void)
+{
+	connG = mysql_init(NULL);
+	if (!mysql_real_connect(connG, host, user, pass, dbname, port, unix_socket, flag))
+	{
+		fprintf(stderr, "Error: %s [%d]\n", mysql_error(connG), mysql_errno(connG));
+		exit(1);
+	}
+	printf("Connection Successfull\n");
+
+	//callback para o sinal ctrl+c para terminar ciclo infinito
+	signal(SIGINT, InterceptCTRL_C);
+
+	//Ciclo infinito
+	START_CYCLE();
+
+	printf("\nDisconnected\n");
+	mysql_close(connG);
+	return 0;
+}
